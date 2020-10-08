c75f6df165b168082ead7e634a59787b46bb1377
Shi <shiltsev_sergei@mail.ru>
2020-10-08 22:30:01 +0300

    lesson05_task02_rev01

--- src/main/java/com.ifmo.course/Lesson05/Task02.java
+++ src/main/java/com.ifmo.course/Lesson05/Task02.java
@@ -1,4 +1,14 @@
 package com.ifmo.course.Lesson05;
 
 public class Task02 {
+    public static void main(String[] args) {
+        // задача 2
+        // Найти количество вхождений одной строки в другую.
+        // Например, строка "дом" встречается в строке "дом домик домой одомашненный" 4 раза
+        /*String predlozhenie = "дом домик домой одомашненный";
+        String iskomoeSlovo = "дом";
+        int domCount = StringUtils.countMatches(predlozhenie, iskomoeSlovo);
+        System.out.println(domCount);*/
+
+    }
 }
