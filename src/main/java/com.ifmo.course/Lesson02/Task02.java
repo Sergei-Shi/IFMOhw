d2e351b98d935830fa9f997751cdc4ef17a48092
Shi <shiltsev_sergei@mail.ru>
2020-10-08 21:01:28 +0300

    lesson02_task02_rev01

--- src/main/java/com.ifmo.course/Lesson02/Task02.java
+++ src/main/java/com.ifmo.course/Lesson02/Task02.java
@@ -1,4 +1,18 @@
 package com.ifmo.course.Lesson02;
 
 public class Task02 {
+    public static void main(String[] args) {
+        //задача 2
+        /*Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….*/
+        System.out.println();
+        System.out.println("Задача 2");
+        int a = 2;
+        int b = 1;
+        do {
+            System.out.println(a);
+            a = a * 2;
+            b = b + 1;
+        } while (b <= 20);
+    }
+
 }
