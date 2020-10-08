67a4334e25ba09460b281366b8c878e33e679489
Shi <shiltsev_sergei@mail.ru>
2020-10-08 21:03:39 +0300

    lesson02_task03_rev01

--- src/main/java/com.ifmo.course/Lesson02/Task03.java
+++ src/main/java/com.ifmo.course/Lesson02/Task03.java
@@ -1,4 +1,15 @@
 package com.ifmo.course.Lesson02;
 
 public class Task03 {
+    public static void main(String[] args) {
+        //задача 3
+        /*Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….*/
+        System.out.println();
+        System.out.println("Задача 3");
+        int c = 90;
+        do {
+            System.out.println(c);
+            c = c - 5;
+        } while (c >= 0);
+    }
 }
